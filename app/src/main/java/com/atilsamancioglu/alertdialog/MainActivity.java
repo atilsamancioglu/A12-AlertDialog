de348a33e882224873a47c4f767bb1997115d969
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-04-14 14:25:31 +0300

    Initial commit

---
+++ app/src/main/java/com/atilsamancioglu/alertdialog/MainActivity.java
@@ -0,0 +1,54 @@
+package com.atilsamancioglu.alertdialog;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Context
+        //Activity Context
+
+        //App Context
+
+        //getApplicationContext()
+
+
+        Toast.makeText(this,"Toast Message",Toast.LENGTH_LONG).show();
+    }
+
+
+    public void save(View view) {
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Save");
+        alert.setMessage("Are you sure?");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //save
+                Toast.makeText(getApplicationContext(),"Saved",Toast.LENGTH_LONG).show();
+
+            }
+        });
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this,"Not Saved",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        alert.show();
+
+    }
+}
